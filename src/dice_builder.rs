5d4b9c5d1ead3d479c9b07a0d8dd57a17896ef58
Brayden Jonsson <92825422+F1scherman@users.noreply.github.com>
2023-11-08 19:11:41 -0700

    Update dice_builder.rs

--- src/dice_builder.rs
+++ src/dice_builder.rs
@@ -5,17 +5,17 @@ pub struct DiceBuilder {
 }
 
 impl DiceBuilder {
-    pub fn sides(&mut self, sides: u32) -> &mut Self {
+    pub fn sides(mut self, sides: u32) -> mut Self {
         self.dice_sides = sides;
         self
     }
 
-    pub fn name(&mut self, name: String) -> &mut Self {
+    pub fn name(mut self, name: String) -> mut Self {
         self.dice_name = name;
         self
     }
 
-    pub fn build(&self) -> dice::Dice {
+    pub fn build(self) -> dice::Dice {
         dice::Dice {
             previous_result: 0,
             sides: self.dice_sides,
