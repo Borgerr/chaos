ee332f8e551d3ca3f2635568b97d994945723916
momoevil <malonepeters123@gmail.com>
2023-11-11 12:01:14 -0700

    Create INSULT.py
    HELP ME

---
+++ INSULT.py
@@ -0,0 +1,17 @@
+import random
+
+def generate_shakespearean_insult():
+    adjectives = ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained", "clapper-clawed", "clay-brained", "common-kissing", "crook-pated", "dismal-dreaming", "dizzy-eyed", "doghearted", "dread-bolted", "earth-vexing", "elf-skinned", "empty-hearted", "evil-eyed", "fat-kidneyed", "fen-sucked", "flap-mouthed", "fly-bitten", "folly-fallen", "fool-born", "full-gorged", "guts-griping", "half-faced", "hasty-witted", "hedge-born", "hell-hated", "idle-headed", "ill-nurtured", "knotty-pated", "milk-livered", "motley-minded", "onion-eyed", "plume-plucked", "pottle-deep", "pribbling", "puking", "puny", "qualling", "rank", "reeling-ripe", "roguish", "rump-fed", "shard-borne", "sheep-biting", "spur-galled", "swag-bellied", "tardy-gaited", "tickle-brained", "toad-spotted", "unchin-snouted", "weather-bitten", "whoreson", "yeasty"]
+
+    nouns = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig", "bugbear", "bum-bailey", "canker-blossom", "clack-dish", "clotpole", "coxcomb", "codpiece", "crutch", "cutpurse", "death-token", "dewberry", "flap-dragon", "flax-wench", "flirt-gill", "foot-licker", "fustilarian", "giglet", "gudgeon", "haggard", "harpy", "hedge-pig", "horn-beast", "hugger-mugger", "joithead", "lewdster", "lout", "maggot-pie", "malt-worm", "mammet", "measle", "minnow", "miscreant", "moldwarp", "mumble-news", "nut-hook", "pigeon-egg", "pignut", "puttock", "pumpion", "ratsbane", "scullion", "skainsmate", "strumpet", "varlot", "vassal", "whey-face", "wagtail"]
+
+    insult = f"Thou {random.choice(adjectives)} {random.choice(nouns)}!"
+    return insult
+
+def main():
+    # Example usage
+    for _ in range(5):
+        print(generate_shakespearean_insult())
+
+if __name__ == "__main__":
+    main()
